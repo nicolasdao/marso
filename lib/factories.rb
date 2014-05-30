ef4412b7fd7d54531c152ad489ea5e55840ae11d
Nicolas Dao <nicolas@quivers.com>
2014-05-30 17:10:03 +1000

    Add new marso.rb file

--- lib/factories.rb
+++ lib/factories.rb
@@ -1,23 +1,22 @@
-require "marso/version"
-# require 'watir-webdriver'
-# require 'selenium-webdriver'
-#
-# module Marso
-#   module_function
-#
-#   def openNewBrowser(browser=nil)
-#     browser = !browser.nil? && browser.class == Symbol ? browser : :firefox
-#     profile = nil
-#     case browser
-#     when :firefox
-#       profile = Selenium::WebDriver::Firefox::Profile.new
-#     else
-#       raise "Marso does not support '#{browser}' yet. The only supported browser is currently firefox"
-#     end
-#
-#     profile['browser.cache.disk.enable'] = false
-#
-#     b = Watir::Browser.new browser, :profile => profile
-#     return b
-#   end
-# end
+require 'watir-webdriver'
+require 'selenium-webdriver'
+
+module Marso
+  module_function
+
+  def openNewBrowser(browser=nil)
+    browser = !browser.nil? && browser.class == Symbol ? browser : :firefox
+    profile = nil
+    case browser
+    when :firefox
+      profile = Selenium::WebDriver::Firefox::Profile.new
+    else
+      raise "Marso does not support '#{browser}' yet. The only supported browser is currently firefox"
+    end
+
+    profile['browser.cache.disk.enable'] = false
+
+    b = Watir::Browser.new browser, :profile => profile
+    return b
+  end
+end
